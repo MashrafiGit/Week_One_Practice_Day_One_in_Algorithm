5f671bf4e5fc8de19fbc0415e48f71191446af92
MashrafiGit <mdmasrafi902@gmail.com>
2025-09-07 09:39:07 +0600

    check rooms can be visited or not

---
+++ keys_and_rooms.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    bool vis[1005];
+
+    void bfs(int src, vector<vector<int>> &rooms)
+    {
+        queue<int> q;
+        q.push(src);
+        vis[src] = true;
+
+        while (!q.empty())
+        {
+            int par = q.front();
+            q.pop();
+
+            cout << par << " ";
+
+            for (auto child : rooms[par])
+            {
+                if (!vis[child])
+                {
+                    q.push(child);
+                    vis[child] = true;
+                }
+            }
+        }
+    }
+
+    bool canVisitAllRooms(vector<vector<int>> &rooms)
+    {
+        memset(vis, false, sizeof(vis));
+
+        bfs(0, rooms);
+
+        for (int i = 0; i < rooms.size(); i++)
+        {
+            if (!vis[i])
+                return false;
+        }
+        return true;
+    }
+};
